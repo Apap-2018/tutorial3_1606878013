27da8d16b5a1fec7f3bdb985ae886d0f0feae7d9
kaelky <rickychandra.j@gmail.com>
2018-09-26 16:51:29 +0700

    Add View Pilot Method with PathVariable

--- src/main/java/com/apap/tutorial3/controller/PilotController.java
+++ src/main/java/com/apap/tutorial3/controller/PilotController.java
@@ -5,10 +5,12 @@ import com.apap.tutorial3.service.PilotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PilotController {
@@ -40,5 +42,16 @@ public class PilotController {
 
         model.addAttribute("listPilot", archive);
         return "viewall-pilot";
+    }
+
+    @RequestMapping("pilot/view/license-number/{licenseNumber}")
+    public String viewPilot(
+            @PathVariable String licenseNumber,
+            Model model)
+    {
+        PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+
+        model.addAttribute("pilot", archive);
+        return "view-pilot";
     }
 }
